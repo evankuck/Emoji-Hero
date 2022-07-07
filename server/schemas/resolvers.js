62a058d62e3bedc1f682be7ff82e9d80c7f65849
tparlmer <tparlmer@gmail.com>
2022-07-07 13:10:04 -0400

    updated database

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+// this will be the module for all our resolvers
+
+import models from "../models"
+
+export default {
+    Query: {
+
+    },
+    Mutation: {
+
+    },
+    User: {
+        id: ID,
+        name: String
+    }
+}
