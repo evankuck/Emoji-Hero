ea6fbda305aad26b1bd3b61b631151362e52b022
tparlmer <tparlmer@gmail.com>
2022-07-11 15:10:17 -0400

    added graphql mutations

--- client/src/components/CellForm/CellForm.js
+++ client/src/components/CellForm/CellForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useMutation, gql } from "@apollo/client";
 import {
   Grommet,
   Form,
@@ -11,11 +12,24 @@ import {
   FormField,
   TextInput,
   Button,
-  TextArea
+  TextArea,
 } from "grommet";
-import { Close, Send, User } from 'grommet-icons';
+import { Close, Send, User } from "grommet-icons";
 
-export const CellForm = ({setOpen, data}) => {
+export const CellForm = ({ setOpen, data }) => {
+  
+  const CREATE_DAY = gql`
+  
+  `;
+
+  const UPDATE_DAY = gql`
+  
+  `;
+  
+  const DELETE_DAY = gql`
+  
+  `;
+  
   return (
     <Form
       onSubmit={({ value }) => {
@@ -23,21 +37,31 @@ export const CellForm = ({setOpen, data}) => {
       }}
     >
       <FormField name="name" htmlFor="textinput-id" label="Choose an Emoji">
-      <Box align="center" pad="large">      
-      <Box direction="row">
-        <Button label="1"></Button>
-        <Button label="2"></Button>
-        <Button label="3"></Button>
-        <Button label="4"></Button>
-        <Button label="5"></Button>
-      </Box>
-  </Box>
+        <Box align="center" pad="large">
+          <Box direction="row">
+            <Button 
+              label="1"
+            ></Button>
+            <Button 
+              label="2"
+            ></Button>
+            <Button 
+              label="3"
+            ></Button>
+            <Button 
+              label="4"
+            ></Button>
+            <Button 
+              label="5"
+            ></Button>
+          </Box>
+        </Box>
       </FormField>
 
-      <FormField name="dailyNote" htmlFor="dailyNote" label="How was your day?">
+      {/* <FormField name="dailyNote" htmlFor="dailyNote" label="How was your day?">
       <TextArea name="dailyNote"/>
-      </FormField>
-    
+      </FormField> */}
+
       <Box direction="row" gap="medium">
         <Button type="submit" primary label="Submit" />
         <Button type="reset" label="Reset" />
