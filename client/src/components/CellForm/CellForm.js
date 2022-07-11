2fbd99285cda495f922b7bfe0bfff164724adb49
Brittany Ganser <15brganser@gmail.com>
2022-07-11 13:22:37 -0600

    added test button values

--- client/src/components/CellForm/CellForm.js
+++ client/src/components/CellForm/CellForm.js
@@ -11,37 +11,42 @@ import {
   FormField,
   TextInput,
   Button,
-  TextArea
+  TextArea,
 } from "grommet";
-import { Close, Send, User } from 'grommet-icons';
 
-export const CellForm = ({setOpen, data}) => {
+
+export const CellForm = ({ setOpen, data }) => {
+  const getEmoji = (e) => {
+    const selectedEmoji = e.target.value;
+    return selectedEmoji;
+  };
+
   return (
     <Form
       onSubmit={({ value }) => {
         console.log(value);
       }}
     >
-      <FormField name="name" htmlFor="textinput-id" label="Choose an Emoji">
-      <Box align="center" pad="large">      
-      <Box direction="row">
-        <Button label="1"></Button>
-        <Button label="2"></Button>
-        <Button label="3"></Button>
-        <Button label="4"></Button>
-        <Button label="5"></Button>
-      </Box>
-  </Box>
+      <FormField name="emoji" htmlFor="emoji-button" label="Choose an Emoji">
+        <Box align="center" pad="large">
+          <Box direction="row">
+            <Button label="1" type="submit" value="emoji-1"></Button>
+            <Button label="2" type="submit" value="emoji-2"></Button>
+            <Button label="3" type="submit" value="emoji-3"></Button>
+            <Button label="4" type="submit" value="emoji-4"></Button>
+            <Button label="5" type="submit" value="emoji-5"></Button>
+          </Box>
+        </Box>
       </FormField>
 
       <FormField name="dailyNote" htmlFor="dailyNote" label="How was your day?">
-      <TextArea name="dailyNote"/>
+        <TextArea name="dailyNote" />
       </FormField>
-    
+
       <Box direction="row" gap="medium">
         <Button type="submit" primary label="Submit" />
         <Button type="reset" label="Reset" />
-        <Button type="close" onBlur={() => setOpen(false)} label="Close" />
+        <Button type="submit" onBlur={() => setOpen(false)} label="Close" />
       </Box>
     </Form>
   );
